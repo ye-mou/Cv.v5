69291395041ea79bf735daf956fc81371b8856a5
ye-mou <git.ymo22@gmail.com>
2022-12-01 23:02:21 +0100

    js for active button, css for button and updated html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 /*=============== FILTERS TABS ===============*/
 
+const tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]') 
+
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tc => { /* tc = tabcontent*/
+            tc.classList.remove('filters_active')
+            
+        })
+
+        target.classList.add('filters_active')
+
+        tab.forEach(t => { /**t = tab */
+            t.classList.remove('filter-tab-active')
+        })
+        tab.classList.add('filter-tab-active')
+    })
+})
 
 /*=============== DARK LIGHT THEME ===============*/
 
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== SCROLL REVEAL ANIMATION ===============*/ 
