0333a12c1ef444921c102f9288056f72f090b679
ye-mou <git.ymo22@gmail.com>
2022-12-02 13:29:33 +0100

    scrollreveal

--- js/main.js
+++ js/main.js
@@ -58,3 +58,18 @@ themeButton.addEventListener('click', () => {
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/ 
+const sr = ScrollReveal({
+origin:'top',
+distance:'60px',
+duration:2500,
+delay:400,
+})
+
+sr.reveal('.profile_border')
+sr.reveal('.profile_name', {delay:500})
+sr.reveal('.profile_profession', {delay:600})
+sr.reveal('.profile_social', {delay:700})
+sr.reveal('.profile_info-group', { interval:100,delay:700}) 
+sr.reveal('.profile_buttons', {delay:800})
+sr.reveal('.profile_content', {delay:900})
+sr.reveal('.filters', {delay:1000})
